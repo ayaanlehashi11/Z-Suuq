f468e66d57673ee7a6bc8cfe9b7aacc677a4c28a
ayanle <ayaanlemohamed540@gmail.com>
2022-10-05 17:17:50 +0300

    init

---
+++ app/src/main/java/com/ayaanle/h_suuq/adapter/CategoryAdapter.kt
@@ -0,0 +1,23 @@
+package com.ayaanle.h_suuq.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> (){
+    inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
